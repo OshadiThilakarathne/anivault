18d7c0258503dcacffe9bf21c37136eaeb5c1d7f
Oshadi Thilakarathne <it24102608@my.sliit.lk>
2026-04-18 19:15:24 +0530

    feat: add AnimeContext with useReducer, localStorage sync, and derived stats

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
-function Home() {
-    return <div>Home</div>
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
 }
-
-export default Home
